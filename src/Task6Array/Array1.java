a376893153075318593587509b4aae8c20930c17
Nikitir <nikitakirjanovs@gmail.com>
2023-03-31 21:59:26 +0300

    Added task 6

---
+++ src/Task6Array/Array1.java
@@ -0,0 +1,14 @@
+package Task6Array;
+
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        int[] myNum = {10, 20, 30, 40};
+        System.out.println(Arrays.toString(cars));
+        System.out.println(cars[0]);
+        System.out.println(Arrays.toString(myNum));
+        System.out.println(myNum[1]);
+    }
+}
